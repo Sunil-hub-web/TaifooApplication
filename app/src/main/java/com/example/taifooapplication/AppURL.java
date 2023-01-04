f02cbdef86f3183b1aef59a07c9680d3ce323aed
sunilNewBranch <sunilkumard628@gmail.com>
2023-01-04 19:10:28 +0530

    changes and updated the url

--- app/src/main/java/com/example/taifooapplication/AppURL.java
+++ app/src/main/java/com/example/taifooapplication/AppURL.java
@@ -2,45 +2,45 @@ package com.example.taifooapplication;
 
 public class AppURL {
 
-    public static final String userRegister = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=register";
+    public static final String userRegister = "https://taifoo.in/admin/api/api.php?method=ac_register";
 
-    public static final String userLogin = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=login";
+    public static final String userLogin = "https://taifoo.in/admin/api/api.php?method=login";
 
-    public static final String getUserDetails = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=getProfile";
+    public static final String getUserDetails = "https://taifoo.in/admin/api/api.php?method=getProfile";
 
-    public static final String updateProfilePic = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=uploadProfilePic";
+    public static final String updateProfilePic = "https://taifoo.in/admin/api/api.php?method=uploadProfilePic";
 
-    public static final String updateProfileDetails = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=UpProfile";
+    public static final String updateProfileDetails = "https://taifoo.in/admin/api/api.php?method=UpProfile";
 
-    public static final String getUserLocation = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=getLocations";
+    public static final String getUserLocation = "https://taifoo.in/admin/api/api.php?method=getLocations";
 
-    public static final String addAddress = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=AddAddress";
+    public static final String addAddress = "https://taifoo.in/admin/api/api.php?method=AddAddress";
 
-    public static final String getAddressDetails = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=getAddress";
+    public static final String getAddressDetails = "https://taifoo.in/admin/api/api.php?method=getAddress";
 
-    public static final String deleteAddressDetails = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=delAddress";
+    public static final String deleteAddressDetails = "https://taifoo.in/admin/api/api.php?method=delAddress";
 
-    public static final String changePassword = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=changePassword";
+    public static final String changePassword = "https://taifoo.in/admin/api/api.php?method=changePassword";
 
-    public static final String forgotPassword = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=forgetpassword";
+    public static final String forgotPassword = "https://taifoo.in/admin/api/api.php?method=forgetpassword";
 
-    public static final String getHomePageDetails = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=getHome";
+    public static final String getHomePageDetails = "https://taifoo.in/admin/api/api.php?method=getHome";
 
-    public static final String addToCart = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=addtoCart";
+    public static final String addToCart = "https://taifoo.in/admin/api/api.php?method=addtoCart";
 
-    public static final String updateToCart = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=updateCart";
+    public static final String updateToCart = "https://taifoo.in/admin/api/api.php?method=updateCart";
 
-    public static final String deleteFormCart = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=removeFromCart";
+    public static final String deleteFormCart = "https://taifoo.in/admin/api/api.php?method=removeFromCart";
 
-    public static final String getItemFormCart = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=getCartItem";
+    public static final String getItemFormCart = "https://taifoo.in/admin/api/api.php?method=getCartItem";
 
-    public static final String getShippingCharges = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=getShipping";
+    public static final String getShippingCharges = "https://taifoo.in/admin/api/api.php?method=getShipping";
 
-    public static final String getProductCategory = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=categorypage";
+    public static final String getProductCategory = "https://taifoo.in/admin/api/api.php?method=categorypage";
 
-    public static final String orderPlaced = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=placeorder";
+    public static final String orderPlaced = "https://taifoo.in/admin/api/api.php?method=placeorder";
 
-    public static final String getOrderDetails = "http://collegeprojectz.com/taifoo/admin/Api/api.php?method=GetOrders";
+    public static final String getOrderDetails = "https://taifoo.in/admin/api/api.php?method=GetOrders";
 
 
 
